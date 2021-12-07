b3e47db4283875c2b933605392d90ada8e2cde29
Karim Said <inkriment@gmail.com>
2021-12-07 23:43:08 +0500

    Implementing the Service classes and the Repository Patter

---
+++ src/BookStore.Infrastucture/Repositories/CategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using BookStore.Infrastucture.Context;
+
+namespace BookStore.Infrastucture.Repositories
+{
+    public class CategoryRepository : Repository<Category>,ICategoryRepository
+    {
+        public CategoryRepository(BookStoreDbContext context):base(context) { }
+    }
+}
